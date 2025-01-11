91903fa0287e154d58f0a0db034e38d4eb2e1ad9
NekoSakuraLucia <mokurolucia@gmail.com>
2025-01-11 18:34:29 +0700

    เพิ่ม MobileNav เพิ่มเติม

--- travel-loading-page/src/components/Navbar.jsx
+++ travel-loading-page/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import { BiSearch, BiUser } from "react-icons/bi";
+import { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const [logo, setLogo] = useState(false);
+
     const data = [
         { href: "/", label: "Home" },
         { href: "/", label: "Destinations" },
@@ -10,8 +15,13 @@ const Navbar = () => {
         { href: "/", label: "Book" },
     ]
 
+    const handleOpen = () => {
+        setNav(!nav);
+        setLogo(!logo)
+    }
+
     return (
-        <div className="flex w-full items-center justify-between h-20 px-4 absolute z-10">
+        <div className="flex w-full items-center justify-between h-20 px-4 lg:px-10 absolute z-10">
             <div>
                 <h2 className="font-bold text-3xl p-3">Travel-Landing</h2>
             </div>
@@ -30,6 +40,33 @@ const Navbar = () => {
             <div className="hidden md:flex gap-10">
                 <BiSearch className="hover:text-blue-400 duration-300 cursor-pointer" size={20} />
                 <BiUser className="hover:text-blue-400 duration-300 cursor-pointer" size={20} />
+            </div>
+
+            <div onClick={handleOpen} className="md:hidden">
+                {nav ? <AiOutlineMenu size={25} /> : <AiOutlineClose size={25} />}
+            </div>
+
+            <div className={
+                nav ?
+                    "absolute left-0 top-0 w-full bg-black/20 backdrop-blur-lg px-4 py-7 flex-flex-col"
+                    : "absolute left-[-100%]"
+            }>
+                <ul>
+                    <h2 className="text-2xl font-bold mb-5">Travel-Landing</h2>
+
+                    {data.map((item, index) => (
+                        <li key={index} className="my-6">
+                            <Link href={item.href} className="text-base font-medium no-underline">
+                                {item.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className="md:hidden flex flex-row items-center justify-center gap-5">
+                    <BiSearch className="hover:text-blue-400 duration-300 cursor-pointer" size={20} />
+                    <BiUser className="hover:text-blue-400 duration-300 cursor-pointer" size={20} />
+                </div>
             </div>
         </div>
     )
