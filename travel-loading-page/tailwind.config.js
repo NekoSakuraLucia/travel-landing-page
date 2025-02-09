71e4f53d8bd1a08354dcaeaacf0f1ae130385bb7
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-09 09:29:31 +0700

    เพิ่ม scroll ใน tailwind config

--- travel-loading-page/tailwind.config.js
+++ travel-loading-page/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'scroll': {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(8px)' }
+        }
+      },
+      animation: {
+        'scroll': 'scroll 1.5s ease-in-out infinite alternate'
+      },
+    },
   },
   plugins: [],
 }
