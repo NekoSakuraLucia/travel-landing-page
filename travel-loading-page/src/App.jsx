836510ec26baa5cf6d83d8f2558382ef2e650e0b
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-09 11:54:40 +0700

    เพิ่มคอมโพเนนต์ Footer แต่ยังไม่ได้ตกแต่ง

--- travel-loading-page/src/App.jsx
+++ travel-loading-page/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import HomePage from "./components/Home/Hero"
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
         </Routes>
       </div>
+      <Footer />
     </Router>
   )
 }
