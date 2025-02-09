fb9be8f3ef429860fd220517ef1a9cd147c27972
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-09 16:22:53 +0700

    จัดระเบียบไฟล์ต่างๆเพิ่ม lint semi และ quotes

--- travel-loading-page/src/App.jsx
+++ travel-loading-page/src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import HomePage from "./components/Home/Hero"
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import HomePage from './components/Home/Hero';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       </div>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
