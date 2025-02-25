1226b3dfdc1ab31ab93a4420a3d1544fca0b092c
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-25 16:27:41 +0700

    เพิ่มคอมโพเนนต์ About

--- travel-loading-page/src/App.jsx
+++ travel-loading-page/src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import HomePage from './components/Home/Hero';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import HomePage from "./components/Home/Hero";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import About from "./components/About/About";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="min-h-screen">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </div>
       <Footer />
